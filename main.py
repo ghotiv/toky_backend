2baa60c4dcf89bf93692a558a3d61d7e64b65589
ghoti <ghoti@ghotis-Mac-mini.local>
2025-09-09 21:12:25 +0800

    g

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 from fastapi import FastAPI
 from typing import Dict, Any
 
+from web3_util import get_decode_calldata
+
 app = FastAPI()
 
 @app.post("/webhook")
 def webhook(data: Dict[str, Any]):
     print(data)
-    print(type(data))
+    calldata = data['event']['data']['block']['logs'][0]['transaction']['inputData']
+    res_decode_calldata = get_decode_calldata(calldata)
+    print(res_decode_calldata)
     return "success"
