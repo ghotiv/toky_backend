ea763a78bd086ca38d98f4f5d7cb74cf6401d015
ghoti <ghoti@ghotis-Mac-mini.local>
2025-09-10 22:42:53 +0800

    g

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 from fastapi import FastAPI
 from typing import Dict, Any
 
-from web3_util import call_fill_relay_by_alchemy
+from web3_util import call_fill_relay_by_alchemy,get_decode_calldata
 
 app = FastAPI()
 
 @app.post("/webhook")
 def webhook(data: Dict[str, Any]):
     print('data: ', data)
-    tx_hash = call_fill_relay_by_alchemy(data)
-    print('tx_hash: ', tx_hash)
+    transaction_dict = data['event']['data']['block']['logs'][0]['transaction']
+    print('depositHash: ', transaction_dict['hash'])
+    # tx_hash = call_fill_relay_by_alchemy(data)
+    # print('tx_hash: ', tx_hash)
     return "success"
