44ba9e3520a29e3a308d59511b421e9d0c0d1268
ghoti <ghoti@ghotis-Mac-mini.local>
2025-09-16 08:03:46 +0800

    g

--- web3_test_tmp.py
+++ web3_test_tmp.py
@@ -13,7 +13,8 @@ def test_get_decode_calldata():
 
 def test_call_deposit(private_key=None,recipient=None):
     # block_chainid = 84532
-    block_chainid = 11155111
+    # block_chainid = 11155111
+    block_chainid = 300
 
     # inputToken = to_checksum_address('0x0000000000000000000000000000000000000000')
     # inputAmount = get_wei_amount(0.0001)
