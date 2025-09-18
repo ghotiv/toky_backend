b8247c1ac3eaa7c0b4ad404d935bd8b67e4fb7e6
ghoti <ghoti@ghotis-Mac-mini.local>
2025-09-18 18:15:56 +0800

    g

--- web3_test_tmp.py
+++ web3_test_tmp.py
@@ -20,10 +20,10 @@ def test_call_deposit(private_key=None,recipient=None):
     # inputAmount = get_wei_amount(0.0001)
     # inputAmount = get_wei_amount(1000)
 
-    # inputToken = get_token(chain_id=block_chainid,token_name='ETH')['token_address']
-    # inputAmount = get_wei_amount(0.001)
-    inputToken = get_token(chain_id=block_chainid,token_name='MBT')['token_address']
+    inputToken = get_token(chain_id=block_chainid,token_name='ETH')['token_address']
     inputAmount = get_wei_amount(0.001)
+    # inputToken = get_token(chain_id=block_chainid,token_name='MBT')['token_address']
+    # inputAmount = get_wei_amount(0.001)
     # inputAmount = get_wei_amount(1)
 
     # destinationChainId = 11155111
