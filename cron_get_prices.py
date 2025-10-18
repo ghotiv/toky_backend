bc2047c3b32be929ab478ec464626b132a8b04bd
ghoti <ghoti@ghotis-Mac-mini.local>
2025-10-18 14:10:10 +0800

    g

--- cron_get_prices.py
+++ cron_get_prices.py
@@ -7,6 +7,8 @@ def main():
         currency_list = ['BTC','ETH','POL','BNB']
         res_prices = get_currency_prices(currency_list,exchange=None)
         for currency,price in res_prices.items():
+            print(time.strftime('%Y-%m-%d %H:%M:%S'))
+            print(f"set {currency} price: {price}")
             set_tmp_price(currency,price)
         time.sleep(60)
 
