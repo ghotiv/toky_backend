22c84c8353013410a6cd35f0d5233522a06a8c27
ghoti <ghoti@ghotis-Mac-mini.local>
2025-10-18 14:16:53 +0800

    g

--- cron_get_prices.py
+++ cron_get_prices.py
@@ -5,12 +5,19 @@ from data_util import get_currency_prices,set_tmp_price,get_tmp_price
 def main():
     while True:
         currency_list = ['BTC','ETH','POL','BNB']
-        res_prices = get_currency_prices(currency_list,exchange=None)
+        res_prices = {}
+        try:
+            res_prices = get_currency_prices(currency_list,exchange=None)
+        except Exception as e:
+            print(time.strftime('%Y-%m-%d %H:%M:%S'))
+            print(f"error: {e}")
+            continue
         for currency,price in res_prices.items():
             print(time.strftime('%Y-%m-%d %H:%M:%S'))
             print(f"set {currency} price: {price}")
             set_tmp_price(currency,price)
         time.sleep(30)
+        print(f"sleep 30 seconds")
 
 if __name__ == '__main__':
-    main()
+    main()  
